2b0db0e46f61c424ac30e1089083f9f258e5459e
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-05-07 14:46:00 -0500

    modify landing page to include responsive text

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,8 +15,8 @@ export default function Home() {
             <source src={BackgroundVideo} type="video/mp4"/>
         </video>
          <div className='full-screen-video-content'>
-            <h1 className='lg:text-9xl md:text-7xl sm:text-5xl py-10 text-3xl text-white text-center center cursive text-2xl'>Hi There, I'm Jeff</h1>
-            <p className='lg: text-7xl md:text5-xl sm:text-3xl text-white text-center cursive'>Welcome to my new portfolio page</p>
+            <h1 className='lg:text-7xl md:text-5xl sm:text-3xl py-10 text-3xl text-white text-center cursive'>Hi There, I'm Jeff</h1>
+            <p className='lg:text-7xl md:text-5xl sm:text-3xl text-white text-center cursive'>Welcome to my new portfolio page</p>
           </div>
         </div>
         );
