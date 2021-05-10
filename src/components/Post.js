db058b20e4c933d1e0437e69c38772b2c4b9bc64
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-05-10 09:30:06 -0500

    remove useless overflow-scroll

--- src/components/Post.js
+++ src/components/Post.js
@@ -34,30 +34,30 @@ export default function Post() {
             <source src={BackgroundVideo} type="video/mp4"/>
         </video>
          <div className='full-screen-video-content'>
-                <section className='content-center text-center px-10 overflow-scroll'>
+                <section className='content-center text-center px-10'>
                     <h1 className='sm: text-xl md: text-2xl lg: text-4xl text-white cursive py-10  z-10'>A journey in front-end engineering</h1>
                     <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8 z-10'>
                         {postData && postData.map((post, index) => (
-                    <article>
-                        <Link to={'/post/' + post.slug.current} key={post.slug.current}>
-                                <span 
-                                    className='block h-30 relative rounded shadow leading-snug bg-red-700 border-l-8 border-green-400 z-10 opacity-75'
-                                    key={index}
-                                >
-                                <img 
-                                    src={post.mainImage.asset.url}
-                                    alt={post.mainImage.alt}
-                                    className='w-full h-full rounded-r object-contain flex justify-end absolute'
-                                />
-                                <span className='block relative h-full flex justify-end items-end pr-1 object-fill'>
-                                    <h3 className='text-gray-800 text-lg font-bold cursive px-3 py-4 text-red-100 rounded'>
-                                        {post.title}
-                                    </h3>
-                                </span>
-                                </span>
-                            </Link>
-                        </article> 
-                        ))}
+                        <article>
+                            <Link to={'/post/' + post.slug.current} key={post.slug.current}>
+                                    <span 
+                                        className='block h-20 relative rounded shadow leading-snug bg-red-700 border-l-8 border-green-400 z-10 opacity-75'
+                                        key={index}
+                                    >
+                                    <img 
+                                        src={post.mainImage.asset.url}
+                                        alt={post.mainImage.alt}
+                                        className='w-full h-full rounded-r object-scale-down flex justify-end absolute'
+                                    />
+                                    <span className='block relative h-full flex justify-end items-end pr-1 object-fill'>
+                                        <h3 className='text-gray-800 text-lg font-bold cursive px-3 py-4 text-red-100 rounded'>
+                                            {post.title}
+                                        </h3>
+                                    </span>
+                                    </span>
+                                </Link>
+                            </article> 
+                            ))}
                     </div>
                     </section>
             </div>
