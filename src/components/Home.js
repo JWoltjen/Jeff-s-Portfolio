359891d9798284b46eddfa5e8673d9eaa127ea7b
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-07-30 03:58:14 -0500

    Revert "Mobile is not working correctly"

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import BackgroundVideo from '../island.mp4';
 
 export default function Home() {
     return (
+
       <div className='full-screen-video-container'>
         <video 
             autoPlay
@@ -13,10 +14,10 @@ export default function Home() {
             className='full-screen-video-container video'>
             <source src={BackgroundVideo} type="video/mp4"/>
         </video>
-          <div className='full-screen-video'>
+         <div className='full-screen-video-content'>
             <h1 className='lg:text-7xl md:text-5xl sm:text-3xl py-10 text-3xl text-white text-center cursive'>Hi There, I'm Jeff</h1>
-            <p className='lg:text-7xl md:text-5xl sm:text-3xl text-white text-center cursive'>Welcome to my portfolio page!</p>
+            <p className='lg:text-7xl md:text-5xl sm:text-3xl text-white text-center cursive'>Welcome to my new portfolio page</p>
           </div>
-      </div>
-  );
+        </div>
+        );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,19 +4,19 @@ import {SocialIcon} from 'react-social-icons'
 
 export default function NavBar() {
     return (
-        <header className="bg-red-600 h-24">
+        <header className="bg-red-600">
             <div className="container mx-auto flex justify-between">
-                <nav className="flex ">
+                <nav className="flex">
                     <NavLink 
                         to="/" 
                         exact 
                         activeClassName="text-white"
-                        className="inline-flex items-center py-4 px-3 mr-4 text-red-100 font-bold cursive tracking-widest sm: text-xs md: text-base lg: text-4xl">
+                        className="inline-flex items-center py-6 px-3 mr-4 text-red-100 font-bold cursive tracking-widest sm: text-xs md: text-base lg: text-6xl">
                         Jeff
                     </NavLink>
                     <NavLink 
                         to="/post"
-                        className="inline-flex items-center py-4 px-3 my-4 rounded text-red-200 hover:text-green-800 cursive sm: text-xs md: text-base lg: text-xl"
+                        className="inline-flex items-center py-6 px-3 my-4 rounded text-red-200 hover:text-green-800 cursive sm: text-xs md: text-base lg: text-xl"
                         activeClassName="text-red-100 bg-red-700"
                         >
 
@@ -24,22 +24,24 @@ export default function NavBar() {
                     </NavLink>
                     <NavLink 
                         to="/project"
-                        className="inline-flex items-center py-4 px-3 my-4 rounded text-red-200 hover:text-green-800 cursive"
+                        className="inline-flex items-center py-6 px-3 my-4 rounded text-red-200 hover:text-green-800 cursive"
                         activeClassName="text-red-100 bg-red-700"
                     >
                         Projects
                     </NavLink>
                     <NavLink 
                         to="/about"
-                        className="inline-flex items-center py-4 px-3 my-4 rounded text-red-200 hover:text-green-800 cursive"
+                        className="inline-flex items-center py-6 px-3 my-4 rounded text-red-200 hover:text-green-800 cursive"
                         activeClassName="text-red-100 bg-red-700"
                     >
                         About Me
                     </NavLink>
-                     <div className="inline-flex p-3 my-6">
-                        <SocialIcon url='https://www.linkedin.com/in/jeff-woltjen-6326b924/' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}}/>
-                    </div>
                 </nav>
+                <div className="inline-flex py-3 px-3 my-6">
+                    <SocialIcon url='https://www.linkedin.com/in/jeff-woltjen-6326b924/' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}}/>
+                    <SocialIcon url='https://www.youtube.com/' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}}/>
+                    <SocialIcon url='https://twitter.com/' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}}/>
+                </div>
             </div>
         </header>
     )
