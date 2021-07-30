ce2f490dbaeef23e1721279a7b2eeaf7afc7fa52
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-30 03:15:41 -0500

    add flex-col to bio

--- src/components/About.js
+++ src/components/About.js
@@ -55,9 +55,9 @@ export default function About() {
             <source src={BackgroundVideo} type="video/mp4"/>
         </video>
          <div className='full-screen-video-content'>
-                <section className='bg-white-200 rounded-lg shadow-2xl lg:flex p-2'>
+                <section className='bg-white rounded-lg lg:flex flex-col p-2'>
                         <h1 className='cursive text-3xl text-red-600 mb-2'>
-                            Hey there. I'm {author.name}
+                            Hey there. I'm {author.name}!
                         </h1>
                         <div className='prose lg:prose-xl text-red-600 cursive'>
                             <BlockContent blocks={author.bio} projectId='8rgqlktn' dataset='production'/>
